40acd7b99ff29fb6048564f99928e4f03ae756bf
Vartika Saxena <vartikasaxena251@gmail.com>
2022-08-22 17:34:50 +0530

    done

--- day12/script.js
+++ day12/script.js
@@ -1,16 +1,41 @@
+// console.log('my name is');
+// localStorage.setItem('name','vartika');//add a key value pair inside the local storage
+
+// localStorage.clear();//clear the entire local storage
+// localStorage.removeItem('name');//remove a particular key value pair
+// let Name= localStorage.getItem('name');//retrive an item from local storage
+// let Name1= localStorage.getItem('namejjkkkklllllllllllllllllll');
+// console.log(Name);
+// console.log(Name1);
+
+
+
+
+
 // Selectors
 var todoInput = document.querySelector(".todo-input");
 var btn = document.querySelector("button");
 var todolist = document.querySelector(".todo-list");
+var i=1;
 
 //Event Handlers
 btn.onclick = create;
 todolist.onclick = deletecheck;
 
+  
+
 //Functions
 function create(e) {
-  if (todoInput.value != "") {
+  // if (todoInput.value != "") {
     e.preventDefault();
+    if(todoInput.value=="")
+  {
+    alert("Input value can t blank");
+  }
+else
+{
+    window.localStorage.setItem("mylist"+i,todoInput.value);
+  i++;
 
     var newDiv = document.createElement("div");
     newDiv.classList.add("todo");
@@ -32,10 +57,8 @@ function create(e) {
     
     todolist.appendChild(newDiv);
     todoInput.value = "";
-  } else {
-    alert("Input Field Can't Be Blank");
-  }
-}
+  } 
+
 
 function deletecheck(e) {
   var item = e.target;
@@ -49,4 +72,10 @@ function deletecheck(e) {
     var parent = item.parentElement;
     parent.classList.toggle("completed");
   }
+}
+}
+
+window.onbeforeunload = function() {
+  localStorage.setItem(i, $('.todo-input').val());
+  
 }
